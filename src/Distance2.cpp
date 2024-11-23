28444fe51d7980725ed072d4b1df793943259756
GitBB3 <beaujean.bertille@orange.fr>
2024-11-23 16:10:54 +0000

    not_stuck_turtles

--- src/Distance2.cpp
+++ src/Distance2.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 double THR=1;
 float dx, dy, dist_cart=THR;
+double x1_mem=0, y1_mem=0, x2_mem=5, y2_mem=5;
 
 turtlesim::Pose pose_turtle1, pose_turtle2;
 geometry_msgs::Twist vel_turtle1, vel_turtle2;
@@ -27,15 +28,18 @@ void turtle1Callback(const turtlesim::Pose::ConstPtr& msg)
 	dist_cart = sqrt(dx*dx + dy*dy);
 	ROS_INFO("Distance@[%f]", dist_cart);
 	
-	if (dist_cart < THR || msg->x > 10.0 || msg->x < 0.0 || msg->y > 10.0 || msg->y < 0.0){
+	if (dist_cart < THR || msg->x > 10.0 || msg->x <= 0.0 || msg->y > 10.0 || msg->y <= 0.0){
 	my_vel1.linear.x = 0.0;
 	my_vel1.linear.y = 0.0;
+	pose_turtle1.x = x1_mem;
+	pose_turtle1.y = y1_mem;
 	ROS_WARN("Turtle1: Collision Risk!");
 	pub_turtle1.publish(my_vel1);
 	//sleep(1);
 	}
 	
-	//pub_turtle1.publish(my_vel1);
+	x1_mem = pose_turtle1.x;
+	y1_mem = pose_turtle1.y;
 	
 	std_msgs::Float32 msg_dist;
 	msg_dist.data = dist_cart;
@@ -55,14 +59,18 @@ void turtle2Callback(const turtlesim::Pose::ConstPtr& msg)
 	dist_cart = sqrt(dx*dx + dy*dy);
 	ROS_INFO("Distance@[%f]", dist_cart);
 	
-	if (dist_cart < THR || msg->x > 10.0 || msg->x < 0.0 || msg->y > 10.0 || msg->y < 0.0){
+	if (dist_cart < THR || msg->x > 10.0 || msg->x <= 0.0 || msg->y > 10.0 || msg->y <= 0.0){
 	my_vel2.linear.x = 0.0;
 	my_vel2.linear.y = 0.0;
+	pose_turtle2.x = x2_mem;
+	pose_turtle2.y = y2_mem;
 	ROS_WARN("Turtle2: Collision Risk!");
 	pub_turtle2.publish(my_vel2);
 	//sleep(1);
 		}
-	//pub_turtle2.publish(my_vel2);
+	
+	x2_mem = pose_turtle2.x;
+	y2_mem = pose_turtle2.y;
 	
 	std_msgs::Float32 msg_dist;
 	msg_dist.data = dist_cart;
